77c6a2a4ee2034575cd0ff3ea2279fb3e24e1ead
SebasTheFish <sebastianq04@gmail.com>
2019-11-25 21:10:51 -0500

    yay

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    //config from firebase console
+    apiKey: "AIzaSyBbmvKvgusXNi2KKoeUAPbk_N0wmLT4asM",
+    authDomain: "tutor4u-b50d1.firebaseapp.com",
+    databaseURL: "https://tutor4u-b50d1.firebaseio.com",
+    projectId: "tutor4u-b50d1",
+    storageBucket: "tutor4u-b50d1.appspot.com",
+    messagingSenderId: "274686198653",
+    appId: "1:274686198653:web:d875ded0d322eb1f108214",
+    measurementId: "G-REL2N90EP6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
 const database = firebase.database();
-database.ref('users/').set({
-    user: 'Matt'
-});
-
 
 function writeUserData(userId, name, email, imageUrl) {
     firebase.database().ref('users/' + userId).set({
